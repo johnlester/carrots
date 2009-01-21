dbfce6b6c7845d171459a97015cc6f07bb055109
John Lester <johnleaster@gmail.com>
2009-01-20 22:21:12 -0500

    second commit

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -49,4 +49,54 @@ class Agent
     self.location.agents(:id.not => self.id)
   end
 
+  # Compiles list of possible actions, and calculates 
+  # utility (desirability) of each one
+  # Scale of final utility for actions
+  # Assume 20 actions, with average utility of 10,000 = 200,000 typical total
+  # Overwhelming (99%): 20,000,000
+  # Very likely (95%): 4,000,000
+  # Likely (60%): 300,000     
+  def possible_actions
+    # Interact with local agents
+    local_agents.each do |local_agent|
+      # Attack
+        # Been attacked before by this agent?
+          # Been attacked recently: 10,000,000
+          # Been attacked in past: 100,000
+          # Never been attacked: 100
+        # How much like/dislike for this agent?
+          # 
+        # How much dislike/like for this agent's species?
+        # Aggressiveness of self (personality trait, typical = 10): 1, 2, 5, 10, 50, 100, 1000, 10000
+        
+      
+      
+    end
+    # Interact with local features
+    
+    # Interact with possessions
+
+    # Attack nearby enemy
+    # Build friendship/alliance
+    # Defend self from attack
+    # Give item/money/info to nearby agent
+    # Trade item/money/info to nearby agent
+    # Investigate local area
+    # Build home/fort
+    # Create item from resources
+    # Gather local resources
+    # Gain local followers (intimidation)
+    # Tell followers what to do
+  end
+
+  def in_combat?
+    if self.attacked_recently?
+      if nearby?(last_attacker)
+        return true
+      # elsif nearby?(last_attacker's allies)
+      end
+    end
+    return false
+  end
+
 end
