59e16fcf4ceb6ab9dd94893442d52628afda27b4
John Lester <johnleaster@gmail.com>
2009-02-14 00:12:31 -0500

    Removed old naming files

--- app/models/name_weighted_element.rb
+++
@@ -1,9 +0,0 @@
-class NameWeightedElement
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :text_element,       String
-  property :weight,             Integer
-  belongs_to :name_element_list
-
-end
