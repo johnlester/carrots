5da193d2ce6c88bd88b626d081ba1c01fbf1dba1
John Lester <johnleaster@gmail.com>
2009-02-15 08:59:23 -0500

    Families of endings for names

--- app/models/name_style.rb
+++ app/models/name_style.rb
@@ -4,56 +4,96 @@ class NameStyle
   property :id, Serial
   
   property :vowels, Yaml
-  property :start_cons, Yaml
-  property :mid_cons, Yaml
+  property :cons, Yaml
+  property :mid_only_cons, Yaml
   property :endings, Yaml
+  property :ending_frequency, Float
 
-  property :max_length, Integer
+  property :length_control, Integer
   
-  STD_WEIGHTS = [0, 0, 0, 0, 1, 1, 2, 2, 5, 10]
-  VOWELS = ['a', 'e', 'a', 'e', 'i', 'o', 'a', 'e', 'i', 'o', 'u', 'ee', 'ea', 'ai', 'ou', 'ar', 'el', 'er', 'oo', 'y', 'ah']
-  START_CONS = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'l', 'm', 'm', 'n', 'p', 'ph', 'qu', 
-    'r', 's', 's', 'sc', 'sl', 'sp', 'st', 'sw', 't', 't', 'th', 'th', 'v', 'w', 'x', 'z']
-  MID_CONS = ['b', 'bb', 'c', 'ck', 'd', 'dg', 'dd', 'f', 'ff', 'g', 'gg', 'h', 'j', 'k', 'l', 'll', 'm', 'm', 
-    'mm', 'n', 'n', 'p', 'pp', 'ph', 'qu', 'r', 's', 's', 'ss', 'sc', 't', 't', 'tt', 'th', 'v', 'w', 'x', 'z']
-  ENDINGS = ['or', 'el', 'us', 'e', 'aka', 'aj', 'o', 'ami', 'iki', 'emi', 'ani', 'i', 'a', 'ola', 'awa']
+  COMMON_WEIGHTS = [1, 100, 200, 500, 800, 1000]
+  UNCOMMON_WEIGHTS = [1, 1, 1, 10, 50, 100, 200]
+  RARE_WEIGHTS = [1, 1, 1, 1, 1, 10, 100]
   
+  COMMON_VOWELS = ['a', 'e', 'i']
+  UNCOMMON_VOWELS = ['o', 'u', 'ee', 'ea', 'ai', 'ou', 'ar','er', 'oo']
+  RARE_VOWELS = ['ae', 'el', 'y', 'ah', 'our', 'uo', 'eer']
+  
+  COMMON_CONS = ['c', 'd', 'f', 'g', 'l', 'm', 'n', 'p', 'r', 's', 't', 'th']
+  UNCOMMON_CONS = ['b', 'h', 'j', 'k', 'st', 'v', 'w']
+  RARE_CONS = ['ph', 'qu', 'sc', 'sl', 'sp', 'sw', 'x', 'z']
+  MID_ONLY_CONS = ['bb', 'ck', 'dg', 'dd', 'ff', 'gg', 'll', 'ng', 'ss', 'tt', 'mm', 'pp']
+  
+  ENDINGS = [ ['aka', 'ake', 'aki', 'ana', 'a', 'e'],
+              ['elle', 'alle', 'omme', 'igne', 'agne', 'isse', 'onne'],
+              ['er', 'or', 'on', 'ot'],
+              ['uela', 'ala', 'ado', 'asco', 'aso', 'os', 'arro', 'ua'],
+              ['ev', 'iv', 'ov', 'oy'],
+              ['esky', 'isky', 'osky', 'ensky', 'asky', 'enko', 'osko', 'onko', 'esko'],
+              ['itz', 'etz', 'izz', 'ezz'],
+              ['us', 'ius'],              
+              ['ini', 'iti', 'izi', 'ia', 'a', 'ani', 'evi'],
+              ['ar', 'evar', 'ivar'], 
+              ['aar', 'aas', 'aan'],
+              ['in', 'ou', 'o', 'u'],
+              ['eh', 'ah', 'ih', 'aj'],
+              ['a'],
+              ['a', 'e', 'o', 'i'] ]
+                
   
   def initialize
-    self.vowels = VOWELS.randomly_weight(STD_WEIGHTS)
-    self.start_cons = START_CONS.randomly_weight(STD_WEIGHTS)
-    self.mid_cons = MID_CONS.randomly_weight(STD_WEIGHTS)
-    self.endings = ENDINGS.randomly_weight(STD_WEIGHTS)
-    self.max_length = 3 + rand(4)
+    self.vowels = COMMON_VOWELS.randomly_weight(COMMON_WEIGHTS)
+    self.vowels.update( UNCOMMON_VOWELS.randomly_weight(UNCOMMON_WEIGHTS) )
+    self.vowels.update( RARE_VOWELS.randomly_weight(RARE_WEIGHTS) )
+
+    self.cons = COMMON_CONS.randomly_weight(COMMON_WEIGHTS)
+    self.cons.update( UNCOMMON_CONS.randomly_weight(UNCOMMON_WEIGHTS) )
+    self.cons.update( RARE_CONS.randomly_weight(RARE_WEIGHTS) )
+    self.mid_only_cons = MID_ONLY_CONS.randomly_weight(UNCOMMON_WEIGHTS)
+    
+    self.endings = ENDINGS.random
+    self.ending_frequency = [0.6, 0.8, 0.9].random
+
+    self.length_control = 1 + rand(4)
+    
     self.save
   end  
-    
+  
+  def mid_cons
+    @mid_cons_cached ||= self.cons.merge(self.mid_only_cons)
+  end 
+      
+  
   def random(capitalized = true)
-    case (2 + rand(self.max_length - 1))
-    when 2
-      pattern = [ [:vowels, :mid_cons],
-                  [:start_cons, :vowels],
-                  [:start_cons, :endings] ].random
-    when 3
-      pattern = [ [:vowels, :mid_cons, :vowels],
-                  [:start_cons, :vowels, :mid_cons],
-                  [:start_cons, :vowels, :endings] ].random
-    when 4
-      pattern = [ [:vowels, :mid_cons, :vowels, :mid_cons],
-                  [:vowels, :mid_cons, :vowels, :endings],
-                  [:start_cons, :vowels, :mid_cons, :endings],
-                  [:start_cons, :vowels, :mid_cons, :vowels] ].random
-    when 5
-      pattern = [ [:vowels, :mid_cons, :vowels, :mid_cons, :vowels],
-                  [:vowels, :mid_cons, :vowels, :mid_cons, :endings],
-                  [:start_cons, :vowels, :mid_cons, :vowels, :mid_cons] ].random
-    when 6
-      pattern = [ [:vowels, :mid_cons, :vowels, :mid_cons, :vowels, :mid_cons],
-                  [:vowels, :mid_cons, :vowels, :mid_cons, :vowels, :endings],
-                  [:start_cons, :vowels, :mid_cons, :vowels, :mid_cons, :endings],
-                  [:start_cons, :vowels, :mid_cons, :vowels, :mid_cons, :vowels] ].random
+    
+    #Start of pattern
+    if rand < 0.75
+      pattern = [:cons, :vowels]
+    else
+      pattern = [:vowels]
+    end
+
+    #Loop zero or more times to build middle of pattern
+    (rand(self.length_control)).times do
+      case pattern[-1]
+      when :vowels
+        pattern << :mid_cons
+        pattern << :mid_cons if rand < 0.25  
+      when :mid_cons
+        pattern << :vowels
+      end
     end
     
+    pattern << :mid_cons if pattern[-1] == :vowels
+    
+    pattern << :endings if rand < self.ending_frequency
+    
+    if pattern[-1] == :mid_cons and pattern[-2] == :mid_cons
+      pattern.slice![-1]
+      pattern << :vowels
+    end
+          
+    # Based on pattern generated above, draw letters
     result = ""
     pattern.each do |element|
       result << self.send(element).random
